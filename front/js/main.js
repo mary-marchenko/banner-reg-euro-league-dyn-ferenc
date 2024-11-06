3fe1ac082e18ada0d80f21ad05386ac7aea544d2
Mari <mari@MacBook-Pro.local>
2024-11-06 10:43:25 +0200

    padding fix

---
+++ front/js/main.js
@@ -0,0 +1,28 @@
+(function init100vh(){
+    function setHeight() {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+    setHeight();
+    window.addEventListener('resize', setHeight);
+})();
+
+document.addEventListener("DOMContentLoaded", function() {
+    function isIOS() {
+        return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+    }
+
+    function isMacOS() {
+        return navigator.platform === 'MacIntel' && !navigator.userAgent.includes('Mobile');
+    }
+
+    let itemMagenta = document.querySelector('.item-magenta');
+    let bannerDate = document.querySelector('.banner__date');
+    let bannerTime = document.querySelector('.banner__time');
+
+    if (!isIOS() && !isMacOS() && itemMagenta && bannerDate && bannerTime) {
+        itemMagenta.classList.add('noios-padding');
+        bannerDate.classList.add('noios-padding');
+        bannerTime.classList.add('noios-padding');
+    }
+});
